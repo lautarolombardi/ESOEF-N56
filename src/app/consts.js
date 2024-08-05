9d1b1cf9f0ab6ce9740ab1472dca0e3aefb837d3
lautarolombardi <lombardi.lautaro@outlook.com>
2024-08-05 18:12:45 -0300

    feat: Julio

--- src/app/consts.js
+++ src/app/consts.js
@@ -1,162 +1,162 @@
 export const HEADER_NAV = [
   {
-    name: 'Novedades',
-    path: '/news',
+    name: "Novedades",
+    path: "/news",
   },
   {
-    name: 'Estudiantes',
-    path: '/students',
+    name: "Estudiantes",
+    path: "/students",
   },
   {
-    name: 'Docentes',
-    path: '/teachers',
+    name: "Docentes",
+    path: "/teachers",
   },
   {
-    name: 'Institucional',
-    path: '/school/about',
+    name: "Institucional",
+    path: "/school/about",
   },
-]
+];
 
 export const INTERESTING_LINKS = [
   {
-    name: 'Educar',
-    link: 'https://www.educ.ar',
+    name: "Educar",
+    link: "https://www.educ.ar",
   },
   {
-    name: 'Canal encuentro',
-    link: 'https://encuentro.gob.ar',
+    name: "Canal encuentro",
+    link: "https://encuentro.gob.ar",
   },
   {
-    name: 'Pelis para compartir',
-    link: 'https://www.youtube.com/playlist?list=PLflDdrs_eX7Hn3lD6cIBjUJ4wDasJCmIu',
+    name: "Pelis para compartir",
+    link: "https://www.youtube.com/playlist?list=PLflDdrs_eX7Hn3lD6cIBjUJ4wDasJCmIu",
   },
   {
-    name: 'Contar',
-    link: 'https://www.cont.ar',
+    name: "Contar",
+    link: "https://www.cont.ar",
   },
   {
-    name: 'ESI',
-    link: 'https://drive.google.com/file/d/1luG06_t6YfVn1GkaljGVTm5tn0GpjNQX/view',
+    name: "ESI",
+    link: "https://drive.google.com/file/d/1luG06_t6YfVn1GkaljGVTm5tn0GpjNQX/view",
   },
   {
-    name: 'Malvinas 40 años',
-    link: 'https://www.facebook.com/media/set/?set=a.746532263100509&type=3',
+    name: "Malvinas 40 años",
+    link: "https://www.facebook.com/media/set/?set=a.746532263100509&type=3",
   },
   {
-    name: 'Escuelas por la cuenca',
-    link: 'https://sites.google.com/view/escuelasporlacuenca/desarrollo-del-programa?authuser=0',
+    name: "Escuelas por la cuenca",
+    link: "https://sites.google.com/view/escuelasporlacuenca/desarrollo-del-programa?authuser=0",
   },
-]
+];
 
 export const CONTACT = [
   {
-    value: '4357-5185',
-    icon: 'phone',
+    value: "4357-5185",
+    icon: "phone",
   },
   {
-    value: 'secundaria56lanus',
-    icon: 'email',
+    value: "secundaria56lanus",
+    icon: "email",
   },
   {
-    value: 'secundaria56documentos',
-    icon: 'email',
+    value: "secundaria56documentos",
+    icon: "email",
   },
   {
-    value: 'Cnel. Osorio 3023 - Lanús Oeste',
-    icon: 'location',
+    value: "Cnel. Osorio 3023 - Lanús Oeste",
+    icon: "location",
   },
-]
+];
 
 export const SOCIAL_LINKS = [
   {
-    user: 'Secundaria Lvi Lanús',
-    link: 'https://www.facebook.com/secundarialvi.lanus.7',
-    icon: 'facebook',
+    user: "Secundaria Lvi Lanús",
+    link: "https://www.facebook.com/secundarialvi.lanus.7",
+    icon: "facebook",
   },
   {
-    user: 'e.e.s.n56polo',
-    link: 'https://instagram.com/e.e.s.n56polo',
-    icon: 'instagram',
+    user: "e.e.s.n56polo",
+    link: "https://instagram.com/e.e.s.n56polo",
+    icon: "instagram",
   },
   {
-    user: 'Polo educativo de villa jardin',
-    link: 'https://www.youtube.com/@PoloEducativodeVillaJardin',
-    icon: 'youtube',
+    user: "Polo educativo de villa jardin",
+    link: "https://www.youtube.com/@PoloEducativodeVillaJardin",
+    icon: "youtube",
   },
-]
+];
 
 export const STUDY_MATERIAL = [
   {
-    title: 'Libros de lectura por año',
-    description: 'Encontrá el material acá',
-    link: 'https://padlet.com/vilarinoandreadiana/programa-de-fortalecimiento-los-imprescindibles-y-sugerencia-o9gd37xrceongefy',
+    title: "Libros de lectura por año",
+    description: "Encontrá el material acá",
+    link: "https://padlet.com/vilarinoandreadiana/programa-de-fortalecimiento-los-imprescindibles-y-sugerencia-o9gd37xrceongefy",
   },
   {
-    title: '25 de mayo en audio',
-    description: 'Carpeta de audios',
-    link: 'https://drive.google.com/drive/folders/1PzG8r7AxttIP9S2xfnZ8og-V_1-p9jSM?usp=sharing',
+    title: "25 de mayo en audio",
+    description: "Carpeta de audios",
+    link: "https://drive.google.com/drive/folders/1PzG8r7AxttIP9S2xfnZ8og-V_1-p9jSM?usp=sharing",
   },
   {
     title:
       '"Crónica de una muerte anunciada" G. Márquez. - Profesor Fabian Gómez',
-    description: 'Carpeta de recursos',
-    link: 'https://drive.google.com/drive/folders/1Gt-9gfvcS2p7mz8xlgKXDEdVZv8arLhI?usp=drive_link',
+    description: "Carpeta de recursos",
+    link: "https://drive.google.com/drive/folders/1Gt-9gfvcS2p7mz8xlgKXDEdVZv8arLhI?usp=drive_link",
   },
   {
-    title: 'El espejo africano',
-    description: 'Descargá el libro en pdf',
-    link: 'https://drive.google.com/file/d/1l8F3OqKMj_kRsbYzJ20w1NaG5HUq7VG0/view?usp=sharing',
+    title: "El espejo africano",
+    description: "Descargá el libro en pdf",
+    link: "https://drive.google.com/file/d/1l8F3OqKMj_kRsbYzJ20w1NaG5HUq7VG0/view?usp=sharing",
   },
   {
-    title: 'Manual de historia',
-    description: 'Descargá el manual de historia de 4to año.',
-    link: 'https://drive.google.com/file/d/1-7e3vbXSnUSlmxN1xLRPEdJD-tpIo5zB/view',
+    title: "Manual de historia",
+    description: "Descargá el manual de historia de 4to año.",
+    link: "https://drive.google.com/file/d/1-7e3vbXSnUSlmxN1xLRPEdJD-tpIo5zB/view",
   },
   {
-    title: 'Ciencias sociales',
-    description: 'Descargá el manual de ciencias sociales de 1er año.',
-    link: 'https://drive.google.com/file/d/1-7XungUc7QP_j2LTjCusgqZkq8crpZoZ/view',
+    title: "Ciencias sociales",
+    description: "Descargá el manual de ciencias sociales de 1er año.",
+    link: "https://drive.google.com/file/d/1-7XungUc7QP_j2LTjCusgqZkq8crpZoZ/view",
   },
   {
-    title: 'Profesor Vidal',
-    description: 'Buscá el material que necesites dado por el profesor Vidal.',
-    link: 'https://drive.google.com/drive/folders/1Is_4N94buRuOOTcCKiOf-U6hMe3Gv2XH',
+    title: "Profesor Vidal",
+    description: "Buscá el material que necesites dado por el profesor Vidal.",
+    link: "https://drive.google.com/drive/folders/1Is_4N94buRuOOTcCKiOf-U6hMe3Gv2XH",
   },
   {
-    title: 'Mapas',
-    description: 'Encontrá variedad de mapas de nuestro país y continente.',
-    link: 'https://www.ign.gob.ar/AreaServicios/Descargas/MapasEscolares',
+    title: "Mapas",
+    description: "Encontrá variedad de mapas de nuestro país y continente.",
+    link: "https://www.ign.gob.ar/AreaServicios/Descargas/MapasEscolares",
   },
   {
-    title: 'Lecturas grabadas',
+    title: "Lecturas grabadas",
     description:
-      'Explorá una selección de lecturas grabadas para mejorar tu experiencia de aprendizaje.',
-    link: 'https://docs.google.com/document/d/164j_q5GtApPy6spLqWbJxgqDit-FO30b/edit?usp=sharing&ouid=111141329733704389804&rtpof=true&sd=true',
+      "Explorá una selección de lecturas grabadas para mejorar tu experiencia de aprendizaje.",
+    link: "https://docs.google.com/document/d/164j_q5GtApPy6spLqWbJxgqDit-FO30b/edit?usp=sharing&ouid=111141329733704389804&rtpof=true&sd=true",
   },
   {
-    title: 'Biblioteca virtual',
+    title: "Biblioteca virtual",
     description:
-      'Explora nuestra biblioteca virtual con recursos educativos, libros digitales y material didáctico.',
-    link: 'https://sites.google.com/view/biblioteca-digital-emilce/inicio?authuser=0',
+      "Explora nuestra biblioteca virtual con recursos educativos, libros digitales y material didáctico.",
+    link: "https://sites.google.com/view/biblioteca-digital-emilce/inicio?authuser=0",
   },
   {
-    title: 'El fantasma de Canterville',
-    description: 'Accedé al archivo pdf del libro.',
-    link: 'https://drive.google.com/file/d/1BJtmNQkNpXcieC0-oNAf0Ey-zbRL_9_A/view?usp=sharing',
+    title: "El fantasma de Canterville",
+    description: "Accedé al archivo pdf del libro.",
+    link: "https://drive.google.com/file/d/1BJtmNQkNpXcieC0-oNAf0Ey-zbRL_9_A/view?usp=sharing",
   },
   {
-    title: 'El fantasma de Canterville (en inglés)',
-    description: 'Accedé al archivo pdf del libro.',
-    link: 'https://drive.google.com/file/d/1v9H0QqfTmaQuka44LXhq3vbHxP2wVSUS/view?usp=sharing',
+    title: "El fantasma de Canterville (en inglés)",
+    description: "Accedé al archivo pdf del libro.",
+    link: "https://drive.google.com/file/d/1v9H0QqfTmaQuka44LXhq3vbHxP2wVSUS/view?usp=sharing",
   },
   {
-    title: 'Martin Fierro',
-    description: 'Accedé al archivo pdf del libro.',
-    link: 'https://drive.google.com/file/d/1AD2jFHIq87AIgYpNdOphKJH_YxTl56Vk/view?usp=sharing',
+    title: "Martin Fierro",
+    description: "Accedé al archivo pdf del libro.",
+    link: "https://drive.google.com/file/d/1AD2jFHIq87AIgYpNdOphKJH_YxTl56Vk/view?usp=sharing",
   },
   {
-    title: 'El matadero',
-    description: 'Accedé al archivo pdf del libro.',
-    link: 'https://drive.google.com/file/d/1tC43vmsI6Sq-V6x3f_Qh-q2krPpWH5lO/view?usp=sharing',
+    title: "El matadero",
+    description: "Accedé al archivo pdf del libro.",
+    link: "https://drive.google.com/file/d/1tC43vmsI6Sq-V6x3f_Qh-q2krPpWH5lO/view?usp=sharing",
   },
-]
+];
