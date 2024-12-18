92b6c9608a9c5fb46fe6fe5bfbdab155d1d2fc21
lautarolombardi <lautaroalombardi22@gmail.com>
2024-12-18 12:26:24 -0300

    feat: shadcn config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: "server",
+  adapter: netlify(),
 });
